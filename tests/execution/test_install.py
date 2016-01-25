92f0d707b61bc460873a4f090745e94c0a80d7c0
Filipe Funenga <fmafunenga@gmail.com>
2016-01-25 21:51:18 +0100

    Improves test for install execution

--- tests/execution/test_install.py
+++ tests/execution/test_install.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import unittest
 import subprocess
 
@@ -6,10 +7,25 @@ import pipgh
 
 class TestInstall(unittest.TestCase):
 
-    def test_execution(self):
-        argv = ['install', 'docopt/docopt']
+    def test_execution_fullname(self):
+        argv = ['install', 'nvie/times']
         pipgh.install(False, argv, dry_run=False, output=False)
         rst = subprocess.check_output(['pip', 'freeze'], stderr=subprocess.PIPE)
+        self.assertTrue(any(l.startswith(u'times'.encode('utf-8'))
+                            for l in rst.split()))
+
+    def test_execution_fullname_ref(self):
+        argv = ['install', 'crsmithdev/arrow', '0.5.3']
+        pipgh.install(False, argv, dry_run=False, output=False)
+        rst = subprocess.check_output(['pip', 'freeze'], stderr=subprocess.PIPE)
+        self.assertTrue(any(l.startswith(u'arrow'.encode('utf-8'))
+                            for l in rst.split()))
+
+    def test_execution_requirements(self):
+        argv = ['install', '-r', 'tests/cli/requirements.txt']
+        pipgh.install(False, argv, dry_run=False, output=False)
+        rst = subprocess.check_output(['pip', 'freeze'], stderr=subprocess.PIPE)
+        #
         self.assertTrue(any(l.startswith(u'docopt'.encode('utf-8'))
                             for l in rst.split()))
 
